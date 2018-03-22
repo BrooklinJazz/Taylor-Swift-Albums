d1fdc610ae1b2569e1705d9b17b0a4c7adebbb97
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-03-22 16:01:16 -0700

    draft album list component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactNative from 'react-native';
+import { View, Text } from 'react-native'
+
+const AlbumList = (props) => {
+    return (
+        <View>
+            <Text>Album List</Text>
+        </View>
+    )
+}
+ 
+export default AlbumList;
