995c29dacf0148f55110207cc72a67cc166da993
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-03-23 13:40:27 -0700

    add TouchableOpacity Button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     const { 
@@ -29,8 +30,13 @@ const AlbumDetail = ({ album }) => {
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
+
             <CardSection>
                 <Image source={{uri: image}} style={imageStyle} />
+            </CardSection>
+
+            <CardSection>
+                <Button/>
             </CardSection>
         </Card>
     )
