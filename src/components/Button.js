353dd14fdcbf8099f27ebce15bee5edace814dae
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-03-23 14:08:24 -0700

    refactor Button to make it more reusable

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress }) => {
     const { buttonStyle, textStyle } = styles
     return (
         <TouchableOpacity
-            onPress={() => {
-                console.log('pressed!');
-            }}
+            onPress={onPress}
             style={buttonStyle} >
             <Text style={textStyle} >Click Me!</Text>
         </TouchableOpacity>
