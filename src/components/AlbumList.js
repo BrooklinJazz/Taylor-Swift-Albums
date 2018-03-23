11b5cb6d8edfa0cab177438caa3f466663760467
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-03-23 13:34:12 -0700

    make content scrollable

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactNative from 'react-native';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail'
 
@@ -21,9 +21,9 @@ class AlbumList extends Component {
     render() {
         console.log(this.state)
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
          )
     }
 }
